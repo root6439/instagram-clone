813e96fbcd0baa517b95670829323d94080cd951
Nicolas Soares Henrique <nicolas.henrique@capgemini.com>
2024-02-29 16:37:21 -0300

    alguns ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { AuthGuardService } from './auth-guard.service';
 import { HomeComponent } from './home/home.component';
 import { AcessoComponent } from './acesso/acesso.component';
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
-export const ROUTES: Routes = [
-  {path: '', component: AcessoComponent},
-  {path: 'home', component: HomeComponent, canActivate: [ AuthGuardService ]}
-]
+export const routes: Routes = [
+  { path: '', component: AcessoComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuardService] },
+];
--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -1,70 +1,69 @@
+import { FormControl } from '@angular/forms';
 import { ProgressoService } from './../../progresso.service';
 import { BdService } from './../../bd-service.service';
-import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import firebase from 'firebase/app';
 import { interval, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import "firebase/database";
+import 'firebase/database';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-incluir-publicacao',
   templateUrl: './incluir-publicacao.component.html',
-  styleUrls: ['./incluir-publicacao.component.css']
+  styleUrls: ['./incluir-publicacao.component.css'],
+  standalone: true,
 })
 export class IncluirPublicacaoComponent implements OnInit {
-
   @Output()
   public atualizarTimeLine: EventEmitter<any> = new EventEmitter<any>();
 
   private email: string;
   private imagem: any;
-  public progressoPublicacao: string = 'pendente'
+  public progressoPublicacao: string = 'pendente';
   public porcentagemUpload: number = 0;
 
-  public formulario: UntypedFormGroup = new UntypedFormGroup({
-    'titulo': new UntypedFormControl(null, [Validators.required])
-  })
+  titulo = new FormControl('');
 
   constructor(
     private bd: BdService,
     private progressoService: ProgressoService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     firebase.auth().onAuthStateChanged((user) => {
       this.email = user.email;
-    })
+    });
   }
 
   public publicar(): void {
     this.bd.incluirPublicacao({
-      titulo: this.formulario.value.titulo,
+      titulo: this.titulo.value,
       email: this.email,
-      imagem: this.imagem
+      imagem: this.imagem,
     });
 
     let acompanhamentoUpload = interval(500);
     let continua = new Subject();
-    let limitador = acompanhamentoUpload.pipe(takeUntil(continua))
+    let limitador = acompanhamentoUpload.pipe(takeUntil(continua));
 
-    continua.next(true)
+    continua.next(true);
     limitador.subscribe(() => {
-      this.porcentagemUpload = Math.round((this.progressoService.estado.bytesTransferred / this.progressoService.estado.totalBytes) * 100)
+      this.porcentagemUpload = Math.round(
+        (this.progressoService.estado.bytesTransferred /
+          this.progressoService.estado.totalBytes) *
+          100
+      );
       this.progressoPublicacao = 'andamento';
       if (this.progressoService.status == 'concluido') {
         continua.next(false);
         this.progressoPublicacao = 'concluido';
         this.atualizarTimeLine.emit();
       }
-    })
-
+    });
   }
 
   public uploadImagem(value: Event): void {
-
     this.imagem = (<HTMLInputElement>value.target).files[0];
-
   }
-
 }
